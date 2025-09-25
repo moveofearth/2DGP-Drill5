8c2cb61982c4dccf96e0f04d3faa4ad37d0f1948
chikyuugaugokidasita <ansvia@naver.com>
2025-09-25 16:00:55 +0900

    필요한 함수들 호출

--- 2DGP-Drill5.py
+++ 2DGP-Drill5.py
@@ -5,12 +5,19 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 
+def handle_events():
+    pass
 
+def update():
+    pass
 
-
-
+def render():
+    pass
 
 while True:
+    handle_events()
+    update()
+    render()
     pass
 
 
