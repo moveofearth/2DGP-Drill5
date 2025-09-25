573b88dd1df57bfb701c41ecc02109e88857f761
chikyuugaugokidasita <ansvia@naver.com>
2025-09-25 15:46:48 +0900

    과제시작

---
+++ 2DGP-Drill5.py
@@ -0,0 +1,13 @@
+from pico2d import *
+
+TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+open_canvas(TUK_WIDTH, TUK_HEIGHT)
+tuk_ground = load_image('TUK_GROUND.png')
+character = load_image('animation_sheet.png')
+
+
+
+
+
+
+close_canvas()
